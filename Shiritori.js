e96ac3bcbc5978a304e8ae8ba04dfef465ca72da
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2023-09-19 22:34:25 +0400

    write play method, that check the word is valid for Shiritori game, which word fisrt letter = last letter next word

--- Shiritori.js
+++ Shiritori.js
@@ -3,6 +3,16 @@ class Shiritori{
           this.words = [];
           this.game_over = false;
     }
+    play(word){
+        if(this.words.length > 0 && word[0] !== this.words[this.words.length - 1].slice(-1)){
+            this.game_over = true;
+            return "game over"
+        } else if(this.words.includes(word)){
+            return "game over"
+        }
 
+        this.words.push(word);
+        return this.words;
+    }
    
 }
