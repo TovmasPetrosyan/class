b7f3c15501f3e6b12954a4a603ef766d8133ab39
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2023-09-20 13:39:55 +0400

    final code, where are animals, there are make sound, eat food and sleep. There are Jungle class.

--- Jungle.js
+++ Jungle.js
@@ -1,27 +1,124 @@
 class Jungle {
+  constructor() {
+    this.animals = [];
+  }
 
+  addAnimal(animal) {
+    this.animals.push(animal);
+  }
+
+  soundOff() {
+    this.animals.forEach((animal) => {
+      animal.makeSound();
+      console.log(`${animal.constructor.name} - Energy Level: ${animal.getEnergy()}`);
+    });
+  }
 }
 class Animal {
+  constructor() {
+    this.energy = 0;
+  }
+  makeSound() {
+    this.energy -= 3;
+  }
+  eat(food) {
+    if (food instanceof Food) {
+      this.energy += 5;
+    }
+  }
+  sleep() {
+    this.energy += 10;
+  }
+  getEnergy() {
+    return this.energy;
+  }
 
 }
 class Food {
-
+  constructor(name, energy) {
+    this.name = name;
+    this.energy = energy;
+  }
 }
-class Tiger {
-
+class Tiger extends Animal {
+  constructor() {
+    super();
+  }
+  sleep() {
+    this.energy += 15;
+  }
+  eat(food) {
+    if (food instanceof Meat) {
+      this.energy += 5;
+      console.log(`${this.constructor.name} is eating ${food.constructor.name}. Energy Level: ${this.energy}`);
+    } else {
+      console.log(`${this.constructor.name} can't eat ${food.constructor.name}.`);
+    }
+  }
 }
-class Monkey {
-
+class Monkey extends Animal {
+  constructor() {
+    super();
+  }
+  makeSound() {
+    this.energy -= 4;
+  }
+  eat(food) {
+    if (food instanceof Food) {
+      this.energy += 2;
+    }
+  }
+  play() {
+    if (this.energy >= 8) {
+      this.energy = -8;
+      return 'Oooo Oooo';
+    } else {
+      return "I'm too tired.";
+    }
+  }
 }
-class Snake {
-
+class Snake extends Animal {
+  constructor() {
+    super();
+  }
 }
-class Fish {
-
+class Fish extends Food {
+  constructor(name, energy) {
+    super(name, energy);
+  }
 }
-class Grain {
-
+class Grain extends Food {
+  constructor(name, energy) {
+    super(name, energy);
+  }
 }
-class Meat {
-
+class Meat extends Food {
+  constructor(name, energy) {
+    super(name, energy);
+  }
 }
+
+// const jungle = new Jungle();
+// const tiger = new Tiger();
+// const monkey = new Monkey();
+// const snake = new Snake();
+// const fish = new Fish();
+// const grain = new Grain();
+// const meat = new Meat();
+
+// jungle.addAnimal(tiger);
+// jungle.addAnimal(monkey);
+// jungle.addAnimal(snake);
+
+// tiger.makeSound();
+// tiger.eat(meat);
+// tiger.sleep();
+
+// monkey.makeSound();
+// monkey.eat(grain);
+// monkey.sleep();
+
+// snake.makeSound();
+// snake.eat(fish);
+// snake.sleep();
+// jungle.soundOff();
